4c76b882b6926513003fc2be4db903d04363499b
Kahlil Fitzgerald <kahlil.fitzgerald@gmail.com>
2020-10-08 07:13:31 -0700

    Added Strengths (#4)
    Co-authored-by: Kahlil Fitzgerald <kahlil.g.fitzgerald@gmail.com>

---
+++ src/components/strength/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import StrengthItem from './strength-item'
+
+const StrengthBase = () => {
+    return (
+        <div>
+            <StrengthItem text='Positivity'></StrengthItem>
+            <StrengthItem text='Includer'></StrengthItem>
+            <StrengthItem text='Individualization'></StrengthItem>
+            <StrengthItem text='Adaptability'></StrengthItem>
+            <StrengthItem text='Connectedness'></StrengthItem>
+        </div>
+    )
+}
+
+export default StrengthBase
