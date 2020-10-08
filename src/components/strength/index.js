f607d8a1fe515697e471b8516c1d757d3e7833ab
Kahlil Fitzgerald <kahlil.fitzgerald@gmail.com>
2020-10-08 08:52:59 -0700

    Fixed positioning and added links (#5)
    Co-authored-by: Kahlil Fitzgerald <kahlil.g.fitzgerald@gmail.com>

--- src/components/strength/index.js
+++ src/components/strength/index.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import StrengthItem from './strength-item'
 
+const LINK_POSITIVITY='https://www.gallup.com/cliftonstrengths/en/252305/positivity-theme.aspx'
+const LINK_CONNECTEDNESS='https://www.gallup.com/cliftonstrengths/en/252197/connectedness-theme.aspx'
+const LINK_INDIVIDUALIZATION='https://www.gallup.com/cliftonstrengths/en/252272/individualization-theme.aspx'
+const LINK_ADAPTABILITY='https://www.gallup.com/cliftonstrengths/en/252146/adaptability-theme.aspx'
+const LINK_INCLUDER='https://www.gallup.com/cliftonstrengths/en/252266/includer-theme.aspx'
+
+
 const StrengthBase = () => {
     return (
         <div>
-            <StrengthItem text='Positivity'></StrengthItem>
-            <StrengthItem text='Includer'></StrengthItem>
-            <StrengthItem text='Individualization'></StrengthItem>
-            <StrengthItem text='Adaptability'></StrengthItem>
-            <StrengthItem text='Connectedness'></StrengthItem>
+            <StrengthItem text='Positivity' link={LINK_POSITIVITY}></StrengthItem>
+            <StrengthItem text='Includer' link={LINK_INCLUDER}></StrengthItem>
+            <StrengthItem text='Individualization' link={LINK_INDIVIDUALIZATION}></StrengthItem>
+            <StrengthItem text='Adaptability' link={LINK_ADAPTABILITY}></StrengthItem>
+            <StrengthItem text='Connectedness' link={LINK_CONNECTEDNESS}></StrengthItem>
         </div>
     )
 }
--- src/components/strength/strength-item.js
+++ src/components/strength/strength-item.js
@@ -2,10 +2,12 @@ import React from 'react'
 import './strength-item.css'
 
 const StrengthItem = (props) => {
-    const {text} = props
+    const { text, link } = props
     const classes = `strength-item ${text.toLowerCase()}`
-    return(
-        <span className={classes}>{text}</span>
+    return (
+        <div className={classes}>
+            <a href={link} target='_blank'><span>{text}</span></a>
+        </div>
     )
 }
 
