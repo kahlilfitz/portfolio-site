d528dbb16ad43b902172cc6c4a032e10c09c5826
Kahlil Fitzgerald <kahlil.fitzgerald@gmail.com>
2023-05-18 14:42:15 -0700

    Change theme (#11)
    * added fish file
    
    * deleted old file
    
    * updated
    
    * new package lock file
    
    ---------
    
    Co-authored-by: Kahlil Fitzgerald <kahlil@theillzfactor.com>

--- src/pages/fish-tracker.js
+++ src/pages/fish-tracker.js
@@ -1,19 +1,6 @@
 import React from 'react'
 import Fish from '../components/fish-tracker/fish'
-import Fish from '../components/fish-tracker/fish'
 import Grid from '@mui/material/Unstable_Grid2'
-import {
-  CssBaseline,
-  ThemeProvider,
-  Typography,
-  createTheme,
-} from '@mui/material'
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-})
 import {
   CssBaseline,
   ThemeProvider,
@@ -49,6 +36,19 @@ const FishTracker = () => {
         </Grid>
       </main>
     </ThemeProvider>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <main>
+        <Typography variant="h1">Fish Tracker</Typography>
+        <Grid container spacing={2}>
+          {Object.keys(fishDict).map(key => (
+            <Grid item xs={4}>
+              {fishDict[key]}
+            </Grid>
+          ))}
+        </Grid>
+      </main>
+    </ThemeProvider>
   )
 }
 
@@ -57,6 +57,15 @@ const FishTracker = () => {
  */
 
 const fishDictDefault = {
+  1: <Fish name="Fish 1" handsPlayed={0} />,
+  2: <Fish name="Fish 2" handsPlayed={0} />,
+  3: <Fish name="Fish 3" handsPlayed={0} />,
+  4: <Fish name="Fish 4" handsPlayed={0} />,
+  5: <Fish name="Fish 5" handsPlayed={0} />,
+  6: <Fish name="Fish 6" handsPlayed={0} />,
+  7: <Fish name="Fish 7" handsPlayed={0} />,
+  8: <Fish name="Fish 8" handsPlayed={0} />,
+  9: <Fish name="Fish 9" handsPlayed={0} />,
   1: <Fish name="Fish 1" handsPlayed={0} />,
   2: <Fish name="Fish 2" handsPlayed={0} />,
   3: <Fish name="Fish 3" handsPlayed={0} />,
