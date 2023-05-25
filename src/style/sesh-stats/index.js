60628e8422cd0db21c3c8b9ceba12bb0d4421822
Kahlil Fitzgerald <kahlil.fitzgerald@gmail.com>
2023-05-25 16:38:21 -0700

    Added theme to sesh-stats page (#13)
    Co-authored-by: Kahlil Fitzgerald <kahlil@theillzfactor.com>

--- src/style/sesh-stats/index.js
+++ src/style/sesh-stats/index.js
@@ -1,3 +1,5 @@
+import { createTheme } from "@mui/material"
+
 const baseDataElementSx = {
   border: '1px solid white',
   width: '10em',
@@ -11,8 +13,15 @@ const dataSx = {
   ...baseDataElementSx,
 }
 
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+})
+
 const SeshStyle = {
   headerSx,
   dataSx,
+  darkTheme,
 }
 export default SeshStyle
